7745f9e0749cb24134bdf6f8881f997984191c72
FedeReyes10 <fedeereyes10@gmail.com>
2026-08-05 21:25:59 -0300

    Add files via upload

--- perfumes.js
+++ perfumes.js
@@ -7,7 +7,8 @@ const perfumes = [
         genero: "Masculino",
         precio: 55000,
         destacado: true,
-        imagen: "imagenes/perfumes/khamrah.jpg"
+        etiqueta: "Más vendido",
+        imagen: "khamrah.jpeg"
     },
 
     {
@@ -17,7 +18,497 @@ const perfumes = [
         genero: "Masculino",
         precio: 56000,
         destacado: true,
-        imagen: "imagenes/perfumes/asad.jpg"
-    }
+        etiqueta: "Recomendado",
+        imagen: "asad.jpg"
+    },
+
+    {
+        id: 3,
+        nombre: "9PM",
+        marca: "Afnan",
+        genero: "Masculino",
+        precio: 60000,
+        destacado: true,
+        etiqueta: "Más vendido",
+        imagen: "9pm.jpeg"
+    },
+
+    {
+        id: 4,
+        nombre: "9PM Elixir EDP",
+        marca: "Afnan",
+        genero: "Masculino",
+        precio: 90000,
+        destacado: true,
+        etiqueta: "Nuevo",
+        imagen: "9pm_elixir_edp.jpeg"
+    },
+
+    {
+        id: 5,
+        nombre: "Amber Oud Gold EDP",
+        marca: "Al Haramain",
+        genero: "Masculino",
+        precio: 75000,
+        destacado: true,
+        etiqueta: "Premium",
+        imagen: "amber_oud_gold_edp.jpeg"
+    },
+    {
+    id: 6,
+    nombre: "Bade'e Honor & Glory",
+    marca: "Lattafa",
+    genero: "Masculino",
+    precio: 55000,
+    destacado: true,
+    etiqueta: "Popular",
+    imagen: "badee_honor_glory.jpeg"
+},
+
+{
+    id: 7,
+    nombre: "Bade'e Oud Black",
+    marca: "Lattafa",
+    genero: "Masculino",
+    precio: 52000,
+    destacado: false,
+    etiqueta: "",
+    imagen: "badee_oud_black.jpeg"
+},
+
+{
+    id: 8,
+    nombre: "Bade'e Oud Sublime",
+    marca: "Lattafa",
+    genero: "Masculino",
+    precio: 55000,
+    destacado: false,
+    etiqueta: "",
+    imagen: "badee_oud_sublime.jpeg"
+},
+
+{
+    id: 9,
+    nombre: "Bharara King",
+    marca: "Bharara",
+    genero: "Masculino",
+    precio: 115000,
+    destacado: true,
+    etiqueta: "Premium",
+    imagen: "bharara_king.jpeg"
+},
+
+{
+    id: 10,
+    nombre: "Erba Pura",
+    marca: "Xerjoff",
+    genero: "Masculino",
+    precio: 85000,
+    destacado: true,
+    etiqueta: "Exclusivo",
+    imagen: "erba_pura.jpeg"
+},
+{
+    id: 11,
+    nombre: "Bade'e Oud Amethyst",
+    marca: "Lattafa",
+    genero: "Masculino",
+    precio: 65000,
+    destacado: false,
+    etiqueta: "",
+    imagen: "badee_oud_amethyst.jpeg"
+},
+
+{
+    id: 12,
+    nombre: "The Kingdom",
+    marca: "Lattafa",
+    genero: "Masculino",
+    precio: 70000,
+    destacado: true,
+    etiqueta: "Nuevo",
+    imagen: "the_kingdom.jpeg"
+},
+
+{
+    id: 13,
+    nombre: "Khamrah Qahwa",
+    marca: "Lattafa",
+    genero: "Masculino",
+    precio: 60000,
+    destacado: true,
+    etiqueta: "Popular",
+    imagen: "khamrah_qahwa.jpeg"
+},
+
+{
+    id: 14,
+    nombre: "Khamrah Dukhan",
+    marca: "Lattafa",
+    genero: "Masculino",
+    precio: 50000,
+    destacado: false,
+    etiqueta: "",
+    imagen: "khamrah_dukhan.jpeg"
+},
+
+{
+    id: 15,
+    nombre: "Asad Green",
+    marca: "Lattafa",
+    genero: "Masculino",
+    precio: 48000,
+    destacado: false,
+    etiqueta: "",
+    imagen: "asad_green.jpeg"
+},
+
+{
+    id: 16,
+    nombre: "Asad Bourbon",
+    marca: "Lattafa",
+    genero: "Masculino",
+    precio: 60000,
+    destacado: true,
+    etiqueta: "Nuevo",
+    imagen: "asad_bourbon.jpeg"
+},
+
+{
+    id: 17,
+    nombre: "Hayaati Black",
+    marca: "Lattafa",
+    genero: "Masculino",
+    precio: 48000,
+    destacado: false,
+    etiqueta: "",
+    imagen: "hayaati_black.jpeg"
+},
+
+{
+    id: 18,
+    nombre: "Hayaati Gold Elixir",
+    marca: "Lattafa",
+    genero: "Masculino",
+    precio: 45000,
+    destacado: false,
+    etiqueta: "",
+    imagen: "hayaati_gold_elixir.jpeg"
+},
+{
+    id: 19,
+    nombre: "Odyssey Limoni EDP",
+    marca: "Armaf",
+    genero: "Masculino",
+    precio: 55000,
+    destacado: true,
+    etiqueta: "Nuevo",
+    imagen: "odyssey_limoni_edp.jpeg"
+},
+
+{
+    id: 20,
+    nombre: "Odyssey Homme White",
+    marca: "Armaf",
+    genero: "Masculino",
+    precio: 65000,
+    destacado: true,
+    etiqueta: "Premium",
+    imagen: "odyssey_homme_white.jpeg"
+},
+
+{
+    id: 21,
+    nombre: "Odyssey Homme",
+    marca: "Armaf",
+    genero: "Masculino",
+    precio: 55000,
+    destacado: false,
+    etiqueta: "",
+    imagen: "odyssey_homme.jpeg"
+},
+
+{
+    id: 22,
+    nombre: "Odyssey Spectra EDP",
+    marca: "Armaf",
+    genero: "Masculino",
+    precio: 55000,
+    destacado: true,
+    etiqueta: "Popular",
+    imagen: "odyssey_spectra_edp.jpeg"
+},
+
+{
+    id: 23,
+    nombre: "Odyssey Sky Mandarin",
+    marca: "Armaf",
+    genero: "Masculino",
+    precio: 55000,
+    destacado: false,
+    etiqueta: "",
+    imagen: "odyssey_sky_mandarin.jpeg"
+},
+
+{
+    id: 24,
+    nombre: "Odyssey Omega",
+    marca: "Armaf",
+    genero: "Masculino",
+    precio: 55000,
+    destacado: false,
+    etiqueta: "",
+    imagen: "odyssey_omega.jpeg"
+},
+
+{
+    id: 25,
+    nombre: "Odyssey Mandarin Sky Elixir",
+    marca: "Armaf",
+    genero: "Masculino",
+    precio: 68000,
+    destacado: true,
+    etiqueta: "Nuevo",
+    imagen: "odyssey_mandarin_sky_elixir.jpeg"
+},
+
+{
+    id: 26,
+    nombre: "Club de Nuit Intense EDT",
+    marca: "Armaf",
+    genero: "Masculino",
+    precio: 88000,
+    destacado: true,
+    etiqueta: "Más vendido",
+    imagen: "club_de_nuit_intense_edt.jpeg"
+},
+
+{
+    id: 27,
+    nombre: "Club de Nuit Untold",
+    marca: "Armaf",
+    genero: "Masculino",
+    precio: 90000,
+    destacado: true,
+    etiqueta: "Premium",
+    imagen: "club_de_nuit_untold.jpeg"
+},
+
+{
+    id: 28,
+    nombre: "Club de Nuit Oud",
+    marca: "Armaf",
+    genero: "Masculino",
+    precio: 85000,
+    destacado: true,
+    etiqueta: "Premium",
+    imagen: "club_de_nuit_oud.jpeg"
+},
+{
+    id: 29,
+    nombre: "Hawas Black",
+    marca: "Rasasi",
+    genero: "Masculino",
+    precio: 68000,
+    destacado: true,
+    etiqueta: "Popular",
+    imagen: "hawas_black.jpeg"
+},
+
+{
+    id: 30,
+    nombre: "Hawas Yellow",
+    marca: "Rasasi",
+    genero: "Masculino",
+    precio: 58000,
+    destacado: false,
+    etiqueta: "",
+    imagen: "hawas_yellow.jpeg"
+},
+
+{
+    id: 31,
+    nombre: "Hawas Ice",
+    marca: "Rasasi",
+    genero: "Masculino",
+    precio: 65000,
+    destacado: true,
+    etiqueta: "Nuevo",
+    imagen: "hawas_ice.jpeg"
+},
+
+{
+    id: 32,
+    nombre: "Hawas Elixir",
+    marca: "Rasasi",
+    genero: "Masculino",
+    precio: 70000,
+    destacado: true,
+    etiqueta: "Premium",
+    imagen: "hawas_elixir.jpeg"
+},
+{
+    id: 33,
+    nombre: "Vulcan Feu",
+    marca: "French Avenue",
+    genero: "Masculino",
+    precio: 62000,
+    destacado: true,
+    etiqueta: "Nuevo",
+    imagen: "vulcan_feu.jpeg"
+},
+
+{
+    id: 34,
+    nombre: "Vulcan",
+    marca: "French Avenue",
+    genero: "Masculino",
+    precio: 62000,
+    destacado: false,
+    etiqueta: "",
+    imagen: "vulcan.jpeg"
+},
+
+{
+    id: 35,
+    nombre: "Liquid Brun",
+    marca: "French Avenue",
+    genero: "Masculino",
+    precio: 90000,
+    destacado: true,
+    etiqueta: "Premium",
+    imagen: "liquid_brun.jpeg"
+},
+
+{
+    id: 36,
+    nombre: "Salvo",
+    marca: "French Avenue",
+    genero: "Masculino",
+    precio: 50000,
+    destacado: false,
+    etiqueta: "",
+    imagen: "salvo.jpeg"
+},
+{
+    id: 37,
+    nombre: "Fakhar Rose Gold",
+    marca: "Lattafa",
+    genero: "Femenino",
+    precio: 58000,
+    destacado: true,
+    etiqueta: "Más vendido",
+    imagen: "fakhar_rose_gold.jpeg"
+},
+
+{
+    id: 38,
+    nombre: "Yara Pink",
+    marca: "Lattafa",
+    genero: "Femenino",
+    precio: 56000,
+    destacado: true,
+    etiqueta: "Más vendido",
+    imagen: "yara_pink.jpeg"
+},
+
+{
+    id: 39,
+    nombre: "Yara Moi",
+    marca: "Lattafa",
+    genero: "Femenino",
+    precio: 65000,
+    destacado: true,
+    etiqueta: "Popular",
+    imagen: "yara_moi.jpeg"
+},
+
+{
+    id: 40,
+    nombre: "Yara Mini (50ml)",
+    marca: "Lattafa",
+    genero: "Femenino",
+    precio: 25000,
+    destacado: false,
+    etiqueta: "",
+    imagen: "yara_mini.jpeg"
+},
+
+{
+    id: 41,
+    nombre: "Haya Pink",
+    marca: "Lattafa",
+    genero: "Femenino",
+    precio: 50000,
+    destacado: false,
+    etiqueta: "",
+    imagen: "haya_pink.jpeg"
+},
+
+{
+    id: 42,
+    nombre: "Sehr Lattafa",
+    marca: "Lattafa",
+    genero: "Femenino",
+    precio: 58000,
+    destacado: true,
+    etiqueta: "Nuevo",
+    imagen: "sehr_lattafa.jpeg"
+},
+
+{
+    id: 43,
+    nombre: "Eclaire",
+    marca: "Lattafa",
+    genero: "Femenino",
+    precio: 68000,
+    destacado: true,
+    etiqueta: "Popular",
+    imagen: "eclaire.jpeg"
+},
+
+{
+    id: 44,
+    nombre: "Her Confession",
+    marca: "Lattafa",
+    genero: "Femenino",
+    precio: 58000,
+    destacado: false,
+    etiqueta: "",
+    imagen: "her_confession.jpeg"
+},
+
+{
+    id: 45,
+    nombre: "Vanilla Freak",
+    marca: "Lattafa",
+    genero: "Femenino",
+    precio: 55000,
+    destacado: true,
+    etiqueta: "Vainilla",
+    imagen: "vanilla_freak.jpeg"
+},
+
+{
+    id: 46,
+    nombre: "Club de Nuit Woman",
+    marca: "Armaf",
+    genero: "Femenino",
+    precio: 87000,
+    destacado: true,
+    etiqueta: "Premium",
+    imagen: "club_de_nuit_woman.jpeg"
+},
+
+{
+    id: 47,
+    nombre: "Yum Yum",
+    marca: "Armaf",
+    genero: "Femenino",
+    precio: 70000,
+    destacado: true,
+    etiqueta: "Nuevo",
+    imagen: "yum_yum.jpeg"
+},
 
 ];
